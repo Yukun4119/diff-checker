2dfa02bfc0eb3dd45f44e4e4f9f46e1ae4ac310d
Yukun Shang <ykshang4119@gmail.com>
2023-01-05 01:03:24 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React, { useState } from "react";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import ReactDiffViewer from 'react-diff-viewer';
+import ReactJson from 'react-json-view'
+
 
 
 
 export default function App() {
   const onChangeOld = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     updateOldCode(e.target.value)
+    updateJsonCode(JSON.parse(e.target.value))
   };
   const onChangeNew = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     updateNewCode(e.target.value)
   };
   const [oldCode, updateOldCode] = useState('');
   const [newCode, updateNewCode] = useState('');
+  let [jsonCode, updateJsonCode] = useState(JSON.parse('{"Hello":"World"}'));
+
   return (
     <>
     <Box
@@ -28,6 +33,7 @@ export default function App() {
       <TextField id="outlined-textarea" label="new" variant="outlined" multiline onChange={onChangeNew} />
     </Box>
       <ReactDiffViewer oldValue={oldCode} newValue={newCode} splitView={true} /> 
+      <ReactJson src={jsonCode} />
       </>
   );
 }
